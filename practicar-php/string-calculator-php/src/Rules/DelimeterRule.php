ff20d2efb0360ddea437cd6da8612b02d029e1b4
amlndz <alejandro.melendez@540deg.com>
2025-02-05 11:42:38 +0100

    Test delimitadores especiales tamaño indefinido

--- practicar-php/string-calculator-php/src/Rules/DelimeterRule.php
+++ practicar-php/string-calculator-php/src/Rules/DelimeterRule.php
@@ -4,14 +4,14 @@ namespace Deg540\StringCalculatorPHP\Rules;
 
 class DelimeterRule implements Rule
 {
-    private static string $REGEXDELIMITER = '/^\/\/(.+)\n/';
+    private static string $SIMPLE_REGEX_DELIMITER = '/^\/\/(.+)\n/';
     public function manage(string|array $string): array {
 
         $delimiter = ",";
 
-        if (preg_match(self::$REGEXDELIMITER, $string, $matches)) {
+        if (preg_match($this::$SIMPLE_REGEX_DELIMITER, $string, $matches)) {
             $delimiter = preg_quote($matches[1], '/');
-            $string = preg_replace(self::$REGEXDELIMITER, '', $string);
+            $string = preg_replace($this::$SIMPLE_REGEX_DELIMITER, '', $string);
         }
 
         $numbers = str_replace("\n", $delimiter, $string);
