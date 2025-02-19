ad0bad45e5e4b556cb4f94fdf49d65968aad9fcc
amlndz <alejandro.melendez@540deg.com>
2025-02-19 10:28:57 +0100

    [1] - Excepcion usuario ya logeado

--- practicar-php/user-login-module-php/src/Application/UserLoginService.php
+++ practicar-php/user-login-module-php/src/Application/UserLoginService.php
@@ -2,14 +2,28 @@
 
 namespace UserLoginService\Application;
 
+use Exception;
 use UserLoginService\Domain\User;
 
 class UserLoginService
 {
+    /**
+     * var User[]
+     */
     private array $loggedUsers = [];
 
+    /**
+     * @throws Exception
+     */
     public function manualLogin(User $user): void
     {
+        $existingUser = array_filter($this->loggedUsers, fn ($loggedUser) => $loggedUser->userName() === $user->userName());
+
+        if(!empty($existingUser))
+        {
+            throw new Exception("User already logged in");
+        }
+
         $this->loggedUsers[] = $user;
     }
 
--- practicar-php/user-login-module-php/tests/Application/UserLoginServiceTest.php
+++ practicar-php/user-login-module-php/tests/Application/UserLoginServiceTest.php
@@ -13,7 +13,7 @@ final class UserLoginServiceTest extends TestCase
     /**
      * @test
      */
-    public function userIsLoggedInManually()
+    public function userIsLoggedInManually(): void
     {
         $user = new User('John Doe');
         $userLoginService = new UserLoginService();
@@ -22,6 +22,23 @@ final class UserLoginServiceTest extends TestCase
         $loggedUsers = $userLoginService->getLoggedUsers();
 
         $this->assertCount(1, $loggedUsers);
+        $this->assertEquals('John Doe', $loggedUsers[0]->username());
+    }
+
+    /**
+     * @test
+     */
+    public function userAlreadyLoggedInManually(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('User already logged in');
+
+        $user = new User('John Doe');
+        $userLoginService = new UserLoginService();
+        $userLoginService->manualLogin($user);
+        $userLoginService->manualLogin($user);
+        $loggedUsers = $userLoginService->getLoggedUsers();
+
         $this->assertEquals('John Doe', $loggedUsers[0]->username());
     }
 
